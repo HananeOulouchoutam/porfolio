e9b454ee089269d0a8e42d7ab5dbd7702857594c
HananeOulouchoutam <hanane.oulouchoutam@eidia.ueuromed.org>
2026-04-18 13:16:40 +0100

    Skills Component done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import About from '../components/About'
+import React from "react";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <About/>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
